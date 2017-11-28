853b119afa6a1ce3e4f6b1b3a76fdbf4af52df7c
Joshua Elder <aresx130@gmail.com>
2017-11-28 11:30:39 -0800

    Added body-parser.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 const app = require('express')()
+const bodyParser = require('body-parser')
 
 const routes = require('./routes/index')
 
 app.set('view engine', 'pug')
 app.set('views', __dirname + '/views')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.use('/', routes)
 app.listen(3000)
