d0050d20e5e1f3870a2cfc3eb1fa9dc19d1f3fcb
Joshua Elder <aresx130@gmail.com>
2017-11-27 17:20:20 -0800

    Set express view engine to pug.

--- src/server.js
+++ src/server.js
@@ -2,5 +2,8 @@ const app = require('express')()
 
 const routes = require('./routes/index')
 
+app.set('view engine', 'pug')
+app.set('views', __dirname + '/views')
+
 app.use('/', routes)
 app.listen(3000)
