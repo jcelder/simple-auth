ab9af0a39a44b8afc79c2e7217368b0d4fcd3d6d
Joshua Elder <aresx130@gmail.com>
2017-11-28 12:42:44 -0800

    Added query to get user by email address.

--- src/models/db/user.js
+++ src/models/db/user.js
@@ -4,4 +4,8 @@ const createUser = (user) => {
   return db.query('INSERT INTO users (email, password) VALUES(${email}, ${password})', user)
 }
 
+const findByEmail = (email) => {
+  return db.query('SELECT * FROM users where email=$1', email)
+}
+
 module.exports = { createUser }
