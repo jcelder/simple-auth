53fc81d7b77638fb00d53c852778bf0a3abb6a5a
Joshua Elder <aresx130@gmail.com>
2017-11-28 13:35:30 -0800

    Add express-session middleware.

--- src/server.js
+++ src/server.js
@@ -1,13 +1,23 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
+const session = require('express-session')
 
 const routes = require('./routes/index')
+
+const sess = {
+  secret: 'secret word do not tell',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true },
+}
 
 app.set('view engine', 'pug')
 app.set('views', __dirname + '/views')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+app.use(session(sess))
 
 app.use('/', routes)
 app.listen(3000)
