53fe055caf066dc8d1691370b46ab69a0cab6e8f
Joshua Elder <aresx130@gmail.com>
2017-11-28 11:29:34 -0800

    Added authentication router to routes handler.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 const users = require('./users')
+const authentication = require('./authentication')
 
 router.get('/', (req, res) => {
   res.send('Dashboard goes here')
 })
 
 router.use('/', users)
+router.use('/', authentication)
 
 module.exports = router
