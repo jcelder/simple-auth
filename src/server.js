9d7acea93b3246761e9255bd24d633f765358cfa
Joshua Elder <aresx130@gmail.com>
2017-11-28 15:30:06 -0800

    Added session name to req.session.

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
 const session = require('express-session')
-
+const middleware = require('./routes/middleware')
 const routes = require('./routes/index')
 
 const sess = {
   secret: 'secret word do not tell',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: true },
+  cookie: { secure: false, maxAge: 60000 },
 }
 
 app.set('view engine', 'pug')
@@ -18,6 +18,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use(session(sess))
+
+app.use((req, res, next) => {
+  req.session.name = 'Test Name Please Ignore'
+  next()
+})
+
+app.use(middleware.addUserToRequest)
 
 app.use('/', routes)
 app.listen(3000)
