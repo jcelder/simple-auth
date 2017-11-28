5186843c3fcdda1b7bfd981482d426b71e2cadb1
Joshua Elder <aresx130@gmail.com>
2017-11-28 11:12:55 -0800

    Modified query string to reduce redundant code.

--- src/models/db/user.js
+++ src/models/db/user.js
@@ -1,8 +1,7 @@
 const db = require('./connection')
 
-const createUser = (email, password) => {
-  return db.query('INSERT INTO users (email, password) VALUES($1, $2)',
-    [email, password])
+const createUser = (user) => {
+  return db.query('INSERT INTO users (email, password) VALUES(${email}, ${password})', user)
 }
 
 module.exports = { createUser }
